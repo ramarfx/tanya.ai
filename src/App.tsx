3306d362d7491cc86a10b0587b525ce006c82137
ramarfx <ramagamiing33@gmail.com>
2024-08-28 17:38:54 +0700

    init apa aja

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import { Button } from './components/ui/button'
+
+function App() {
+
+  return (
+    <div className="container">
+      <div className="flex flex-col w-full h-screen justify-center items-center">
+        <h1>Welcome to Tanya.ai</h1>
+        <Button>Test Shadcn</Button>
+      </div>
+    </div>
+  )
+}
+
+export default App
